72378844e5f505f3d92ccb6f47b632d0e26bb057
watanabe-ta826 <takuro_watanabe@cm.jip.co.jp>
2025-11-07 13:57:57 +0900

    3章画像

--- scenes/Chapter3-1Scene.js
+++ scenes/Chapter3-1Scene.js
@@ -1,31 +1,49 @@
-import BaseChapterScene from './BaseChapterScene.js';
-import NPC from '../NPC.js';
-import Portal from '../Portal.js';
-import { gameData } from '../data/game-data.js';
+import BaseChapterScene from "./BaseChapterScene.js";
+import NPC from "../NPC.js";
+import Portal from "../Portal.js";
+import { gameData } from "../data/game-data.js";
 
 export default class Chapter3_1Scene extends BaseChapterScene {
-    constructor() {
-        super('Chapter3-1Scene');
-    }
+  constructor() {
+    super("Chapter3-1Scene");
+  }
 
-    create() {
-        const sceneData = gameData.scenes['Chapter3-1Scene'];
-        super.create(sceneData);
+  create() {
+    const sceneData = gameData.scenes["Chapter3-1Scene"];
+    super.create(sceneData);
 
-        sceneData.entities.forEach(entityData => {
-            let entity;
-            switch (entityData.type) {
-                case 'NPC':
-                    entity = new NPC(this, entityData.x, entityData.y, entityData.name, entityData.dialog, entityData.isStatic, entityData.quiz);
-                    this.physics.add.collider(entity, this.platforms);
-                    break;
-                case 'Portal':
-                    entity = new Portal(this, entityData.x, entityData.y, 50, 100, entityData.targetScene, entityData.entryX);
-                    break;
-            }
-            if (entity) {
-                this.entities.add(entity);
-            }
-        });
-    }
+    sceneData.entities.forEach((entityData) => {
+      let entity;
+      switch (entityData.type) {
+        case "NPC":
+          entity = new NPC(
+            this,
+            entityData.x,
+            entityData.y,
+            entityData.name,
+            entityData.dialog,
+            entityData.isStatic,
+            entityData.quiz,
+            null,
+            entityData.imageName
+          );
+          this.physics.add.collider(entity, this.platforms);
+          break;
+        case "Portal":
+          entity = new Portal(
+            this,
+            entityData.x,
+            entityData.y,
+            50,
+            100,
+            entityData.targetScene,
+            entityData.entryX
+          );
+          break;
+      }
+      if (entity) {
+        this.entities.add(entity);
+      }
+    });
+  }
 }
